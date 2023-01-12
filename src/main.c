3559e82d11f890dc60234d9889826c1c549c6c27
rajh-phuyal <81531002+rajh-phuyal@users.noreply.github.com>
2023-01-12 02:06:01 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -22,11 +22,6 @@ int	main(int argc, char **argv)
 		if (!clean_inputs(1, argc, argv, pigeons) || is_stack_sorted(pigeons))
 			return (free_pigeons(pigeons));
 		pigeons->size_a = stack_size(pigeons->head_a);
-		while (pigeons->head_a)
-		{
-			ft_printf("values : %i\n", pigeons->head_a->value);
-			pigeons->head_a = pigeons->head_a->next;
-		}
 		free_pigeons(pigeons);
 		ft_printf("INPUTS OK\n");
 	}
